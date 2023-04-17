4bf66488a163ce4a5a0ec19de45d33a295fd97d3
Pini Clavero <maxi.a.clavero@gmail.com>
2023-04-17 20:28:07 -0300

    video presentacion funcionando

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "./Action.css";
 import ReactPlayer from "react-player";
+import videoPortada from "../assets/portada.mp4"
 
 
 function Action() {
     return (
         <>
-        <div className="action">
-            <ReactPlayer className="videoEmpresa" muted={true} url="/imagenes/engranajes.mp4" playing loop height="100%" width="100%"/>
-            <div className="cta">
-                <h2>Somos una familia que crece, para ofrecerles un servicio superior.</h2>
-                <button>¿Querés ser parte de la cámara?</button>
-            </div>
+            <div className="action">
+                <div class="videoEmpresa"><video src={videoPortada} autoPlay muted loop></video></div>
+                <div className="cta">
+                    <h2>Somos una familia que crece, para ofrecerles un servicio superior.</h2>
+                    <button>¿Querés ser parte <br></br> de la cámara?</button>
+                </div>
 
-        </div>
+            </div>
         </>
     );
 }
